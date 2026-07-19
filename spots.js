c001ea847a70e4036ece9eff1a53931a3f87bb1d
sakuranbo <sakuranbo547@gmail.com>
2026-07-19 20:56:09 +0900

    トップページを更新

---
+++ spots.js
@@ -0,0 +1,38 @@
+const spots = [
+
+    {
+        name: "ビーチ",
+
+        description: "海を背景にした開放感のある撮影スポット",
+
+        images: {
+            day: "images/test.png",
+            night: "images/test_night.png"
+        }
+    },
+
+
+    {
+        name: "チャペル",
+
+        description: "クラシカルで上品な雰囲気の撮影スポット",
+
+        images: {
+            day: "images/aaa.png",
+            night: "images/test_night.png"
+        }
+    },
+
+
+    {
+        name: "ガーデン",
+
+        description: "自然に囲まれたナチュラルな撮影スポット",
+
+        images: {
+            day: "images/test_night.png",
+            night: "images/garden-night.jpg"
+        }
+    }
+
+];
