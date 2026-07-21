259d125464605212eb8f3ed8eb5c38cb3338dc44
sakuranbo <sakuranbo547@gmail.com>
2026-07-21 20:10:31 +0900

    トップページを更新

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-// スポットを表示する場所
 const spotList = document.querySelector("#spot-list");
 
-
-// spots.jsのデータからカードを作成
 spots.forEach(spot => {
 
-    // カードを作成
     const card = document.createElement("div");
 
     card.className = "spot-card";
 
-
-    // カードのHTMLを作成
     card.innerHTML = `
 
         <img
@@ -47,41 +41,26 @@ spots.forEach(spot => {
 
     `;
 
-
-    // カードを画面に追加
     spotList.appendChild(card);
 
-
-    // 作成した画像を取得
     const image = card.querySelector(".spot-image");
 
-
-    // 作成したタブを取得
     const tabs = card.querySelectorAll(".card-tab");
 
-
-    // 昼・夜のタブにクリックイベントを設定
     tabs.forEach(tab => {
 
         tab.addEventListener("click", function () {
 
-            // day または night を取得
             const time = this.dataset.time;
 
-
-            // 画像を変更
             image.src = spot.images[time];
 
-
-            // すべてのタブからactiveを削除
             tabs.forEach(tab => {
 
                 tab.classList.remove("active");
 
             });
 
-
-            // クリックしたタブをactiveにする
             this.classList.add("active");
 
         });
